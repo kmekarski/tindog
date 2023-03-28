58367c0224a7ea006421fff88cc3eca0b4e1d33a
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-03-28 23:28:48 +0200

    final

--- Dog.js
+++ Dog.js
@@ -3,6 +3,32 @@ class Dog {
     constructor(data) {
         Object.assign(this, data)
     }
+
+    getDogHtml() {
+
+        document.querySelector('.dog-card').style.backgroundImage = `url(/${this.avatar})`
+
+        return `
+            <div class="profile-image">
+                <img class="badge" id="badge-like" src="/images/badge-like.png" alt="">
+                <img class="badge" id="badge-nope" src="/images/badge-nope.png" alt="">
+                <p class="profile-name">${this.name}, ${this.age}</p>
+                <p class="profile-desc">${this.bio}</p>
+            </div> 
+        `
+    }
+
+    swipe() {
+        this.hasBeenSwiped = true
+    }
+
+    accept() {
+        this.hasBeenLiked = true
+    }
+
+    reject() {
+        this.hasBeenLiked = false
+    }
 }
 
 export default Dog
--- index.js
+++ index.js
@@ -1,2 +1,57 @@
-import  dogsData from "/data.js"
+import  dogsDataCopy from "/data.js"
 import Dog from "/Dog.js"
+
+
+const dogsData = dogsDataCopy
+let buttonActive = true
+let dog = new Dog(dogsData.shift())
+render()
+
+
+
+document.addEventListener('click', (e) => {
+    if(e.target.id === "accept-button" || e.target.id === "accept-button-icon" && buttonActive === true) {
+        buttonActive = false;
+        swipeAccept()
+    }
+    if(e.target.id === "reject-button" || e.target.id === "reject-button-icon" && buttonActive === true) {
+        buttonActive = false;
+        swipeReject()
+    }
+})
+
+function newDog() {
+    if(dogsData.length > 0) {
+        setTimeout( () => {
+            buttonActive = true;
+            dog = new Dog(dogsData.shift())
+            hideBadges()
+            render()
+        },1000)
+    }
+}
+
+function swipeAccept() {
+    console.log("swipe")
+    document.getElementById('badge-like').style.visibility = "visible"
+    document.getElementById('badge-nope').style.visibility = "hidden"
+    dog.accept()
+    newDog()
+}
+
+function swipeReject() {
+    document.getElementById('badge-nope').style.visibility = "visible"
+    document.getElementById('badge-like').style.visibility = "hidden"
+    dog.reject()
+    newDog()
+}
+
+function render() {
+    document.querySelector(".dog-card").innerHTML = dog.getDogHtml()
+}
+
+function hideBadges() {
+    document.getElementById('badge-like').style.visibility = "hidden"
+    document.getElementById('badge-nope').style.visibility = "hidden"
+}
+
